f31b0e36043b92a81f29f13e941de26b0ae39754
Yuumei <shjesna@protonmail.com>
2026-05-03 21:49:34 +0200

    quick save for pushing to github

--- vmake.h
+++ vmake.h
@@ -3,11 +3,43 @@
 
 #include <mcu/core.h>
 
+/// Module ID
+typedef usize MID;
+
+typedef enum {
+   MT_Executable
+} ModuleType;
+
 typedef struct {
-   
+   MID id;
+   ModuleType type;
+   String name;
+   Vector MID_dep;
+} Module;
+
+typedef struct {
+   Vector Modules;
 } Vmake;
 
 extern Vmake vmake;
+
+Vmake Vmake_new();
+void Vmake_go_rebuild_yourself(i32 argc, cstr argv[]);
+
+#endif
+
+#ifdefine VMAKE_IMPL
+#undef VMAKE_IMPL
+
+Vmake Vmake_new() {
+   Vmake self = {
+      .Modules = Vector_new(sizeof(Module))
+   };
+   
+   return self;
+}
+
+void Vmake_go_rebuild_yourself(i32 argc, cstr argv[]);
 
 #endif
 
